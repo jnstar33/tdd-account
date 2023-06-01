3a9221d97eabb428f5b11529d9cb06339e3ea1d9
Jinwoo <jnstar@gmail.com>
2023-06-01 14:26:56 +0900

    implement interest functions
    Signed-off-by: Jinwoo <jnstar@gmail.com>

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -21,3 +21,21 @@ TEST_F(AcountFixture, Withdraw) {
 	account.withdraw(600);
 	EXPECT_EQ(9400, account.getBalance());
 }
+TEST_F(AcountFixture, CompoundInterest) {
+	account.applyCompoundInterest(5);
+	EXPECT_EQ(10500, account.getBalance());
+}
+TEST_F(AcountFixture, InterestSetter) {
+	account.setInterest(3);
+	EXPECT_EQ(3, account.getInterest());
+}
+TEST_F(AcountFixture, CompoundInterestWithBankInterest) {
+	account.setInterest(10);
+	account.applyCompoundInterest(10);
+	EXPECT_EQ(11000, account.getBalance());
+}
+TEST_F(AcountFixture, getBalanceAfterNYears) {
+	account.setInterest(10);
+	account.getBalanceAfterNYears(5);
+	EXPECT_EQ(16105, account.getBalance());
+}
--- tdd-bank/cal.cpp
+++ tdd-bank/cal.cpp
@@ -20,6 +20,30 @@ public:
 		balance -= money;
 	}
 
+	void applyCompoundInterest(int interest)
+	{
+		balance += (balance * interest * 0.01);
+	}
+
+	void setInterest(int userInterest)
+	{
+		interest = userInterest;
+	}
+	int getInterest()
+	{
+		return interest;
+	}
+
+	int getBalanceAfterNYears(int n)
+	{
+		for (int i = 0; i < n; i++) {
+			applyCompoundInterest(interest);
+		}
+
+		return balance;
+	}
+
 private:
 	int balance = 10000;
+	int interest = 0;
 };
