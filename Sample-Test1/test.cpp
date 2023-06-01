a8daf30a75bcdeff9b91b55be7734dc6ddae8d00
Jinwoo <jnstar@gmail.com>
2023-06-01 14:02:47 +0900

    add initial code
    Signed-off-by: Jinwoo <jnstar@gmail.com>

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,6 +1,23 @@
 #include "pch.h"
+#include "../tdd-bank/cal.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+using namespace std;
+
+class AcountFixture : public testing::Test
+{
+public:
+	Account account{ 10000 };
+
+};
+
+TEST_F(AcountFixture, CreateAccountInit10000won) {
+	EXPECT_EQ(10000, account.getBalance());
+}
+TEST_F(AcountFixture, Deposit) {
+	account.deposit(500);
+	EXPECT_EQ(10500, account.getBalance());
+}
+TEST_F(AcountFixture, Withdraw) {
+	account.withdraw(600);
+	EXPECT_EQ(9400, account.getBalance());
 }
--- tdd-bank/bank.cpp
+++
@@ -1,5 +0,0 @@
-class Bank
-{
-public:
-
-};
